9201ad7885c9f24e8f1a913dd651cf22fcc20902
Rick Markus <rick@dzap.net>
2015-06-08 14:37:00 -0700

    js tweak

--- js/main.js
+++ js/main.js
@@ -3,21 +3,13 @@ $(document).ready(function($){
 	winHeight = window.innerHeight;
 	winWidth = window.innerWidth;
 
-	console.log (winHeight);
-	console.log (winWidth);
-
 	$('button').hover( //this game is not very smooth, needs work David.
 		function () {
 			var $this = $(this);
 			var roll = Math.random();
-			if(roll > 0.5){
-				$this.css('top', winHeight * Math.random());
-				console.log('higher!');
-			} else {
-				$this.css('left', winWidth * Math.random());
-				console.log('lower!');
-			}
+			$this.css('top', winHeight * roll);
+			//$this.css('left', winWidth * roll);
 		}
-	)
+	);
 
 });
